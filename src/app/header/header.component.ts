820b5037eb964799c19707ab7d381fbd2d1622c3
Tyler Velasco <velastm@dds.dillards.net>
2017-10-13 13:05:25 -0500

    New Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -26,8 +26,31 @@ export class HeaderComponent implements OnInit {
   constructor(element: ElementRef, config: NgbDropdownConfig) {
     this.element = element.nativeElement;
     config.autoClose = true;
-  }
+    document.addEventListener('DOMContentLoaded', function () {
 
+      // Get all "navbar-burger" elements
+      let $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(function ($el) {
+          $el.addEventListener('click', function () {
+
+            // Get the target from the "data-target" attribute
+            let target = $el.dataset.target;
+            let $target = document.getElementById(target);
+
+            // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+            $el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+
+          });
+        });
+      }
+    });
+  }
   ngOnInit(): void {
     this.rect = this.element.getBoundingClientRect();
 
@@ -38,7 +61,6 @@ export class HeaderComponent implements OnInit {
 
     window.addEventListener('scroll', this.scroll.bind(this));
   }
-
   scroll(ev) {
     const yOffset = window.window.pageYOffset;
     this.headerClass = yOffset > this.rect.height ? 'scrolled' : '';
@@ -54,19 +76,6 @@ export class HeaderComponent implements OnInit {
         this.active = section.id;
       }
     }
-  }
-
-  isActive (i) {
-    if (this.active === ('section-' + i)) {
-      return 'active';
-    }
-    return '';
-  }
-
-  scrollTo(ev, i) {
-    ev.preventDefault();
-    const dest = document.getElementById(`section-${i}`);
-    scrollTo(dest.offsetTop, 500);
   }
 
 }
